521d2942b33c0a25805db1a7fb01eb092be616c9
David Fowler <davidf@pcprofessor.edu>
2024-01-24 09:15:12 -0500

    Chaper 3 mods

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const app = express()
 const path = require('path')
+
+const app = express()
 app.use(express.static('public'))
 
 app.listen(3000,()=>{
@@ -8,17 +9,17 @@ app.listen(3000,()=>{
 })
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'index.html'))
+    res.sendFile(path.resolve(__dirname,'pages/index.html'))
 })
 
 app.get('/about',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'about.html'))
+    res.sendFile(path.resolve(__dirname,'pages/about.html'))
 })
 
 app.get('/contact',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'contact.html'))
+    res.sendFile(path.resolve(__dirname,'pages/contact.html'))
 })
 
-app.get('/*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'notfound.html'))
+app.get('/post',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/post.html'))
 })
